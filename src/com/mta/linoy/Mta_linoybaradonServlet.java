4da4aec772c213452e03785f8eb76d2228a1eb4f
linoy <linoybaradon@gmail.com>
2014-11-08 19:47:51 +0200

    result

--- src/com/mta/linoy/Mta_linoybaradonServlet.java
+++ src/com/mta/linoy/Mta_linoybaradonServlet.java
@@ -7,7 +7,18 @@ import javax.servlet.http.*;
 public class Mta_linoybaradonServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		int num1;
+		int num2;
+		int num3;
+		num1=4;
+		num2=7;
+		num3=3;
+		int result= (num1+num3)*num2;
+
+		String resultStr = new String("<h1>Result of ("+num1+"+"+num3+")*"+num2+" ="+result+"</h1>");
+		
+		resp.getWriter().println(resultStr);
+
 	}
 }
